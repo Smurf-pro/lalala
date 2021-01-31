673763a2a5a54497e50ec7ec98817b0ffb96920c
Smurf <isip_i.s.vasilev@mpt.ru>
2021-01-31 17:46:36 +0300

    Commit!

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6
+{
+    public class Class1
+    {
+        //blBLABLA
+        //fghgh
+
+    }
+}
